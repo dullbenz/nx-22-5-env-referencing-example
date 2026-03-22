6c66379fc2197997aa484180e9fb6de2f8698a27
dullbenz <lotbill.fuashi@gmail.com>
2026-03-22 20:23:29 +0100

    feat: add test library and env reference target

---
+++ apps/nx-22-5/scripts/test-env-referencing.js
@@ -0,0 +1,2 @@
+console.log('Will not resolve:', process.env.WILL_NOT_RESOLVE); // Displays an empty string
+console.log('Will resolve:', process.env.WILL_RESOLVE); // Display 22.5.2
---
+++ libs/test/src/lib/test.spec.ts
@@ -0,0 +1,7 @@
+import { test } from './test.js';
+
+describe('test', () => {
+  it('should work', () => {
+    expect(test()).toEqual('test');
+  });
+});
